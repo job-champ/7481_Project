b8ea63c8d8b8bbb714591912e38b166c1c7d2a29
job-champ <job_champ_007@hotmail.com>
2015-11-27 18:26:13 -0600

    Update HTML, CSS, and add JS files for the interface

---
+++ Search.js
@@ -0,0 +1,22 @@
+var text1;
+
+document.getElementById('button').onclick = function()
+{
+	text1 = document.getElementById('myTextArea').value;
+};
+
+
+function giveTextToChetsProcess()
+{
+
+}
+
+function retrieveDataFromChetsProcess()
+{
+
+}
+
+function displayData()
+{
+
+}
