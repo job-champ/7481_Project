2e1cfe5b2029a3e991b6bef9722f416dc8f41982
Job Champagne <jcham49@lsu.edu>
2015-11-30 09:02:41 -0600

    Add new pages for the model and corpus, model has an example wordcloud, while corpus is the beginning of building a wordcloud from the webservice

---
+++ Search.js
@@ -0,0 +1,38 @@
+var text1;
+
+var testString;
+
+var obj;
+
+document.getElementById('button').onclick = function()
+{
+	text1 = document.getElementById('myTextArea').value;
+
+	var r = new XMLHttpRequest(); 
+	r.open("GET", "http://localhost:8082/oracle/getAuthorWords", true);
+	r.onreadystatechange = function () {
+    		if (r.readyState != 4 || r.status != 200) return; 
+                        testString = r.responseText;
+    			console.log(r.responseText);
+			alert(testString);
+		};
+	r.send("a=1&b=2&c=3");
+
+};
+
+function sendTextToWebService()
+{
+
+}
+
+function retrieveDataFromChetsProcess()
+{
+
+}
+
+function displayData()
+{
+
+}
+
+
